185a5fbe34b0d545cc4085d2d3c9531ff53b1436
Leelsmuth <femianjorin@rocketmail.com>
2019-10-04 11:35:38 +0100

    installed dev dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("shows a specific word", () => {
+  const elementWithRose = tools.queryByText(/rose/i);
+  expect(elementWithRose).toBeInTheDocument();
 });
